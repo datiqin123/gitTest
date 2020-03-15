6136ff8ccae60019d55d62bcd505f48e534a9f4f
lwh <527410995@qq.com>
2020-03-15 19:03:02 +0800

    提交

---
+++ src/com/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.git;
+
+/**
+ * @author lwh
+ * @create 2020-03-15 18:21
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println(args);
+        System.out.println(args);
+        System.out.println(args);
+        System.out.println(args);
+    }
+}
